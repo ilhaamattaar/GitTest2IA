156121f6cb776587e1fca47d3677ec48260dc019
ilhaamattaar <ilhaamattaar@gmail.com>
2025-12-03 12:09:25 -0500

    Commit Multi
    commit multi

--- GitTest.java
+++ GitTest.java
@@ -1,3 +1,53 @@
+
+import java.util.concurrent.*;
+import java.io.*;
+//Ilhaam and Mihika December 3
 public class GitTest{
+	static class EditFiles implements Runnable{
+		private String name;
+
+		public EditFiles(String f){
+			this.name = f;
+			}
+
+		@Override
+		public void run(){
+			try(BufferedReader reader = new BufferedReader(new FileReader(name))){
+				String line;
+				while((line = reader.readLine()) != null){
+					String modLine = line.toUpperCase();
+				}
+
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+
+		}
+}
+
+public static void main(String[] args){
+long start = System.nanoTime();
+
+new EditFiles("Frankstein.txt").run();
+new EditFiles("MobyDick.txt").run();
+
+long end = System.nanoTime();
+System.out.println("Time taken(ms): "+(end-start));
+
+long startMulti= System.nanoTime();
+ExecutorService executor = Executors.newFixedThreadPool(2);
+executor.execute(new EditFiles("Frankstein.txt"));
+executor.execute(new EditFiles("MobyDick.txt"));
+executor.shutdown();
+try{
+	executor.awaitTermination(1, TimeUnit.MINUTES);
+}catch(InterruptedException e){
+	e.printStackTrace();
+}
+long endMulti = System.nanoTime();
+System.out.println("Time Taken MultiThread(ms): "+(endMulti-startMulti));
+
+}
+
 
 }
